8e025d88d165ad4edd1a089dc1d8f96f2bc33d92
Eric Willenson <eric.willenson@gmail.com>
2026-03-08 15:39:08 -0400

    Move to wxt

---
+++ entrypoints/background.ts
@@ -0,0 +1,35 @@
+export default defineBackground(() => {
+  console.log('Hello background!', { id: browser.runtime.id });
+});
+
+let windowId = null;
+
+async function openAnnotationWindow() {
+  if (windowId !== null) {
+    try {
+      const win = await browser.windows.get(windowId);
+      if (win) {
+        await browser.windows.update(windowId, { focused: true });
+        return;
+      }
+    } catch (e) {
+      windowId = null;
+    }
+  }
+
+  const win = await browser.windows.create({
+    url: browser.runtime.getURL("window.html"),
+    type: "popup",
+    width: 900,
+    height: 600
+  });
+  windowId = win.id;
+}
+
+browser.windows.onRemoved.addListener((id) => {
+  if (id === windowId) {
+    windowId = null;
+  }
+});
+
+browser.browserAction.onClicked.addListener(openAnnotationWindow);
